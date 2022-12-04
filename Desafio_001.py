86f4206755d90b3b107d12c2dcb05710b3418136
Jefferson Souto <inayan@outlook.com>
2022-12-03 23:57:15 -0300

    Aprendizado em Python do Básico ao Avançado

---
+++ Desafio_001.py
@@ -0,0 +1,3 @@
+print('------DESAFIO 01------')
+nome = input('Qual seu nome? ')
+print('Olá {}! Prazer em te conhecer!'.format(nome))
---
+++ Desafio_003.py
@@ -0,0 +1,5 @@
+print('------DESAFIO 3------')
+num1 = int(input('Digite um número:'))
+num2 = int(input('Digite um segundo número:'))
+s = num1 + num2
+print('A soma entre os números {} e {} é {}'.format(num1,num2,s))
